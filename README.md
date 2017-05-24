f7098002cbd44b8c4a7e79df287c3e049574135b
iwhiteday <idacascos@gmail.com>
2017-05-24 11:03:57 +0200

    converting script modified seem to be continued modifiyng script to more "ruby-way", but atm it looks good

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
-# README
+# It's all about not to lose money
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+Test task, exchange money and chips persistently
--- app/models/chip.rb
+++ app/models/chip.rb
@@ -3,12 +3,19 @@ class Chip < ApplicationRecord
 def self.convert_from_money(amount)
   chips = Chip.where('count > 0').to_a
   values = chips.pluck(:value)
+  counts = chips.map { |c| [c.value, c.count] }.to_h
   result = chips.map { |c| [c.value, 0] }.to_h
-
   if values.include?(amount)
     amount
   else
-    while amount > 0 do
+    result = try_optimal(values, amount, counts, result)
+  end
+  result
+end
+
+class << self
+  private def optimal(values, amount, result)
+    while amount > 0
       values.each do |x|
         if amount >= x
           amount -= x
@@ -16,9 +23,39 @@ def self.convert_from_money(amount)
         end
       end
     end
+    result
   end
 
-  result
+  private def greedy(values, amount, counts, result, index)
+    if amount.zero?
+      result
+    elsif index == -1
+      nil
+    else
+      while amount >= values[index] && counts[values[index]] > 0
+        amount -= values[index]
+        result[values[index]] += 1
+        counts[values[index]] -= 1
+      end
+      greedy(values, amount, counts, result, index - 1)
+    end
+  end
+
+  private def try_optimal(values, amount, counts, result)
+    # might look stupid but i'll take it
+    default_result = result.clone
+    result = optimal(values, amount, result)
+    unless result_possible?(result, counts)
+      result = default_result
+      result = greedy(values, amount, counts, result, values.length - 1)
+    end
+    result
+  end
+
+  private def result_possible?(result, counts)
+    subtraction = result.zip(counts).map { |x,y| y.last - x.last }
+    subtraction.min >= 0
+  end
 end
 
 end
